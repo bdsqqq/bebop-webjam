9b36c21e4e074e92e7fe07c9e265cc91fdb36e04
Igor Bedesqui <igorbedesqui@gmail.com>
2021-10-11 23:34:52 -0300

    feat: updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { mauveDark, redDark } = require("@radix-ui/colors");
+
 module.exports = {
   mode: "jit",
   purge: ["*.html", "*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...mauveDark,
+      ...redDark,
+    },
     extend: {},
   },
   variants: {},
